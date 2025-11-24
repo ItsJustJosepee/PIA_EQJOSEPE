f7e4c62b8d1ad2d721620eb0e0be7390c4ae2f5c
José Barajas <83657397+ItsJustJosepee@users.noreply.github.com>
2025-11-23 21:30:48 -0600

    cambios xd

--- Program.cs
+++ Program.cs
@@ -1,7 +1,5 @@
 using Microsoft.EntityFrameworkCore;
 using PIA_EQJOSEPE.Models;
-using Microsoft.EntityFrameworkCore;
-using BD_Juegos.Models; // ? Usa SOLO donde esté tu BdJuegosContext
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,15 +8,15 @@ builder.Services.AddControllersWithViews();
 
 // Conexión a SQL Server (usa tu cadena llamada "conexion")
 builder.Services.AddDbContext<BdJuegosContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("conexion")));
+	options.UseSqlServer(builder.Configuration.GetConnectionString("conexion")));
 
 var app = builder.Build();
 
 // Configuración pipeline
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
-    app.UseHsts();
+	app.UseExceptionHandler("/Home/Error");
+	app.UseHsts();
 }
 
 app.UseHttpsRedirection();
@@ -27,7 +25,7 @@ app.UseRouting();
 app.UseAuthorization();
 
 app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+	name: "default",
+	pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
